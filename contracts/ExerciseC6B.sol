9560a0f36492c4302c9ab17757e75a6e34915e59
kylesf <kyle.stewartfrantz@gmail.com>
2019-03-12 11:39:31 -0700

    Add C6B

---
+++ contracts/ExerciseC6B.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.25;
+
+// It's important to avoid vulnerabilities due to numeric overflow bugs
+// OpenZeppelin's SafeMath library, when used correctly, protects agains such bugs
+// More info: https://www.nccgroup.trust/us/about-us/newsroom-and-events/blog/2018/november/smart-contract-insecurity-bad-arithmetic/
+
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract ExerciseC6B {
+    using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+
+
+    address private contractOwner;                  // Account used to deploy contract
+
+
+    constructor() public
+    {
+        contractOwner = msg.sender;
+    }
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner()
+    {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+    mapping(address => uint256) private sales;
+    uint256 private enabled = block.timestamp;
+    uint256 private counter = 1;
+
+    modifier rateLimiter(uint time) {
+        require(block.timestamp >= enabled, "Rate timing in effect");
+        enabled = enabled.add(time);
+        // Enter code for this modifier
+        _;
+    }
+
+    modifier entrancyGuard() {
+        // Enter code for this modifier
+        counter = counter.add(1);
+        uint256 guard = counter;
+        _;
+        // Enter code for this modifier
+        require(guard == counter, "The call is not allowed");
+    }
+
+    function safeWithdraw(uint256 amount) external rateLimiter(30) entrancyGuard {
+        //Checks
+        // contracts have tx.origin value
+        require(msg.sender == tx.origin, "Contracts not allowed");
+        require(sales[msg.sender] >= amount, "Insufficient funds");
+        //Effects
+        uint256 withrawnAmount = sales[msg.sender];
+        sales[msg.sender] = sales[msg.sender].sub(amount);
+        //Interaction
+        msg.sender.transfer(withrawnAmount);
+    }
+
+}
+
