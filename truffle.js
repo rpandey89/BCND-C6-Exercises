067a745a80758c382bde161f07e493f72977a9d7
Rahul Pandey <rhl1555@gmail.com>
2019-05-11 20:44:13 -0700

    Exercise c6 a (#1)
    * Add MNEMONIC reminder
    
    * Implementing contract pause and multi-party consensus algo

---
+++ truffle.js
@@ -0,0 +1,16 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+// Be sure to match this mnemonic with that in Ganache!
+var mnemonic = '';
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/', 0, 10)
+      },
+      network_id: '*',
+      gas: 1999999
+    }
+  }
+}
