be70afefab01d90fd71af767860899aa92315b10
Rahul Pandey <rhl1555@gmail.com>
2019-05-25 20:49:00 -0700

    Exercise c6 d (#4)
    * Add C6D
    
    * Adding oracle for request submit flight status

--- truffle.js
+++ truffle.js
@@ -1,13 +1,14 @@
 var HDWalletProvider = require('truffle-hdwallet-provider')
 
 // Be sure to match this mnemonic with that in Ganache!
-var mnemonic = ''
+var mnemonic =
+  'doll source together survey box meat assist green sand conduct ostrich ginger'
 
 module.exports = {
   networks: {
     development: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/', 0, 10)
+        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/', 0, 50)
       },
       network_id: '*',
       gas: 9999999
